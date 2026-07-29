dce65a4f91558ff004bb0558e7d3c3466b80a2fa
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2026-07-29 19:39:49 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -19,5 +19,10 @@ export default {
     <main class="flex-1 flex flex-col min-h-0 pt-22">
       <router-view />
     </main>
+    <div
+      class="w-full py-6 text-main-dark/80 mt-20 flex justify-center items-center border-t border-main-dark/30 text-sm"
+    >
+      Designed & built by Jacob Dunbar © 2026
+    </div>
   </div>
 </template>
