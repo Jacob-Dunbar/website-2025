c19c587dee5918cbbf232bb11a5f126e98d97cd5
Jacob-Dunbar <jacob.dunbar@googlemail.com>
2026-07-31 20:06:29 +0100

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <script>
 import AppNavbar from "./components/AppNavbar.vue";
 import Todo from "./components/Todo.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     Todo,
     AppNavbar,
+    Footer,
   },
 };
 </script>
 
 <template>
-  <div class="fixed inset-0 -z-10 bg-[#f0eee6]"></div>
-
-  <div class="min-h-screen flex flex-col">
+  <div class="min-h-screen flex flex-col !bg-main-dark">
     <Todo />
     <AppNavbar />
     <main class="flex-1 flex flex-col min-h-0 pt-22">
       <router-view />
     </main>
-    <div
-      class="w-full py-6 text-main-dark/80 mt-20 flex justify-center items-center border-t border-main-dark/30 text-sm"
-    >
-      Designed & built by Jacob Dunbar © 2026
-    </div>
+    <Footer />
   </div>
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      expanded: true,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.checkScroll, { passive: true });
+    this.checkScroll();
+  },
+
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.checkScroll);
+  },
+
+  methods: {
+    checkScroll() {
+      const scrollBottom = window.scrollY + window.innerHeight;
+      const pageHeight = document.documentElement.scrollHeight;
+
+      this.expanded = scrollBottom >= pageHeight - 10;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="flex items-end bg-main-dark text-main-light/80 justify-center py-10 md:py-20 md:mt-20">
+    <div
+      :class="[
+        'overflow-hidden whitespace-nowrap transition-all duration-900 ease-in-out',
+        expanded ? 'w-[190px] md:w-[690px]' : 'w-[0px]',
+      ]"
+    >
+      <h1 class="text-3xl md:text-[110px] leading-none">Jacob Dunbar</h1>
+    </div>
+    <div
+      class="bg-main-light/80 mb-[2px]"
+      :class="[
+        ' bg-main-dark transition-all duration-600 ease-in-out',
+        expanded ? 'size-[6px] md:size-[16px] rotate-90 md:mb-[4px]' : 'size-[20px] md:size-[40px] mb-[10px]',
+      ]"
+    ></div>
+  </div>
+</template>
